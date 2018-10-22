20236b27ae0c0527a8e7a57b17174d622178fafe
= <=>
2018-10-22 16:39:31 +0700

    added schema null in schema Tugas

--- src/api/model/Tugas.ts
+++ src/api/model/Tugas.ts
@@ -4,11 +4,11 @@ const TugasSchema = new Schema({
     _id : { type : Schema.Types.ObjectId },
     judul : { type : String, required : true },
     keterangan : { type : String, required : true },
-    bab1 : { type : String },
-    bab2 : { type : String },
-    bab3 : { type : String },
-    bab4 : { type : String },
-    bab5 : { type : String },
+    bab1 : { type : String, default : '' },
+    bab2 : { type : String, default : '' },
+    bab3 : { type : String, default : '' },
+    bab4 : { type : String, default : '' },
+    bab5 : { type : String, default : '' },
 })
 
 export default model('Tugas', TugasSchema);
