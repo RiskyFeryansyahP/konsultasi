0eec63be77731d3eb568fd759178b5792aac1da1
= <=>
2018-10-26 22:22:25 +0700

    added interface into dosen schema

--- src/api/model/Dosen.ts
+++ src/api/model/Dosen.ts
@@ -1,4 +1,14 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model, Document } from 'mongoose'
+
+// membuat interface schema untuk Dosen
+export interface IDosen extends Document {
+    _id : Schema.Types.ObjectId,
+    firstName : string,
+    lastName : string,
+    code : number,
+    meet : Schema.Types.ObjectId,
+    mahasiswa : Schema.Types.ObjectId
+}
 
 //  Membuat Model Schema untuk Dosen
 const DosenSchema = new Schema({
@@ -6,9 +16,10 @@ const DosenSchema = new Schema({
     firstName : { type : String, required : true },
     lastName :  { type : String, required : true },
     code :      {type : Number, required : true},
+    meet : { type : Schema.Types.ObjectId, ref : 'Meet' },
     mahasiswa : [
         { type : Schema.Types.ObjectId, ref : 'Mahasiswa' }
     ]
 })
 
-export default model('Dosen', DosenSchema)
+export default model<IDosen>('Dosen', DosenSchema)
