feb10330f610dea5231a012a66f76fb1a3cd98ac
= <=>
2018-10-26 22:23:28 +0700

    added interface into user schema

--- src/api/model/User.ts
+++ src/api/model/User.ts
@@ -1,13 +1,23 @@
-import { Schema, model } from 'mongoose'
+import * as mongoose from 'mongoose'
+
+// membuat interface model for user
+export interface IUser extends mongoose.Document {
+    _id : mongoose.Types.ObjectId,
+    username : string,
+    password : string,
+    email : string,
+    status : mongoose.Types.ObjectId,
+    onStatus : string
+}
 
 //  Membuat Model Schema untuk User
-const UserSchema = new Schema({
-    _id : { type : Schema.Types.ObjectId },
+const UserSchema = new mongoose.Schema({
+    _id : { type : mongoose.Schema.Types.ObjectId },
     username : { type : String, required : true, unique : true },
     password : { type : String, required : true },
     email :     { type : String, required : true},
-    status : { type : Schema.Types.ObjectId, required : true, refPath: 'onStatus' },
+    status : { type : mongoose.Schema.Types.ObjectId, required : true, refPath: 'onStatus' },
     onStatus : { type : String, required : true, enum : ['Mahasiswa', 'Dosen'] }
 })
 
-export default model("User", UserSchema)
+export default mongoose.model<IUser>("User", UserSchema)
